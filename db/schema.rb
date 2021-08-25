542ccc42beda8a1e2d9d5c779603f553674241f0
helman101 <helmanandresortegon@gmail.com>
2021-08-25 11:17:00 +0200

    rename users password column to password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_102642) do
+ActiveRecord::Schema.define(version: 2021_08_25_091418) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
@@ -32,7 +35,7 @@ ActiveRecord::Schema.define(version: 2021_08_24_102642) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
