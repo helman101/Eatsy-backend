2bb3ead9ef3dba6c5f8fdc73d61081a58c7884ec
helman101 <helmanandresortegon@gmail.com>
2021-09-15 13:42:20 +0200

    test foods controller

--- spec/requests/foods_spec.rb
+++ spec/requests/foods_spec.rb
@@ -1,13 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe 'Orders', type: :request do
+RSpec.describe 'Foods', type: :request do
   let!(:foods) { create_list(:food, 5) }
-  let!(:users) { create_list(:user, 5) }
-  let!(:orders) { create_list(:order, 5, customer_id: users.first.id, food_id: foods.first.id) }
-  let(:order_id) { Order.first.id }
+  let(:food_id) { Food.first.id }
 
-  describe 'GET /orders' do
-    before { get '/orders' }
+  describe 'GET /foods' do
+    before { get '/foods' }
 
     it 'returns order' do
       expect(json).not_to be_empty
@@ -19,13 +17,13 @@ RSpec.describe 'Orders', type: :request do
     end
   end
 
-  describe 'GET /orders/:id' do
-    before { get "/orders/#{order_id}" }
+  describe 'GET /foods/:id' do
+    before { get "/foods/#{food_id}" }
 
-    context 'when the order exist' do
-      it 'returns the order' do
+    context 'when the food exist' do
+      it 'returns the food' do
         expect(json).not_to be_empty
-        expect(json['data']['id']).to eq(order_id)
+        expect(json['data']['id']).to eq(food_id)
       end
 
       it 'returns status code 200' do
@@ -33,29 +31,29 @@ RSpec.describe 'Orders', type: :request do
       end
     end
 
-    context 'when the order doesn\'t exist' do
-      let(:order_id) { 1000 }
+    context 'when the food doesn\'t exist' do
+      let(:food_id) { 1000 }
 
       it 'returns status code 404' do
         expect(response).to have_http_status(404)
       end
 
       it 'returns a not found message' do
-        expect(response.body).to match(/order not found/)
+        expect(response.body).to match(/food not found/)
       end
     end
   end
 
-  describe 'POST /orders' do
+  describe 'POST /foods' do
     let(:correct_attributes) do
-      { order: { customer_id: users.first.id, food_id: foods.first.id, delivered: false } }
+      { food: { name: 'Apple', price: 15, description: 'Red fruit' } }
     end
 
     context 'when the order is valid' do
-      before { post '/orders', params: correct_attributes }
+      before { post '/foods', params: correct_attributes }
 
       it 'creates an order' do
-        expect(json['data']['customer_id']).to eq(users.first.id)
+        expect(json['data']['name']).to eq('Apple')
       end
 
       it 'returns status code 201' do
@@ -64,7 +62,7 @@ RSpec.describe 'Orders', type: :request do
     end
 
     context 'when the order is invalid' do
-      before { post '/orders', params: { order: { food_id: 1, delivered: true } } }
+      before { post '/foods', params: { food: { price: 15, description: 'Red fruit' } } }
 
       it 'returns status code 422' do
         expect(response).to have_http_status(422)
@@ -72,20 +70,20 @@ RSpec.describe 'Orders', type: :request do
 
       it 'return a validation failure message' do
         expect(response.body)
-          .to match(/order not save/)
+          .to match(/food not save/)
       end
     end
   end
 
-  describe 'DELETE /orders/:id' do
-    before { delete "/orders/#{order_id}" }
+  describe 'DELETE /foods/:id' do
+    before { delete "/foods/#{food_id}" }
 
     it 'returns status code 204' do
       expect(response).to have_http_status(200)
     end
 
     it 'returns a confirm message' do
-      expect(response.body).to match(/Deleted order/)
+      expect(response.body).to match(/Deleted food/)
     end
   end
 end
